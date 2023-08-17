b9a14bf6b4be2481a9b448c405227f6f03413f34
Subhajit Sahu <wolfram77@gmail.com>
2023-08-17 22:33:23 +0530

    :bug: update run script

--- main.sh
+++ main.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
-#SBATCH --nodes=4
-#SBATCH --ntasks=4
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=64
-#SBATCH --exclusive
-#SBATCH --mem=30gb
-#SBATCH --job-name slurm
-#SBATCH --output=slurm.out
-# source scl_source enable gcc-toolset-11
-# module load hpcx-2.7.0/hpcx-ompi
-# module load openmpi/4.1.5
-src="hello-mpi"
+src="hello-cuda"
 out="$HOME/Logs/$src$1.log"
-module load hpcx-2.7.0/hpcx-ompi
 ulimit -s unlimited
 printf "" > "$out"
 
@@ -28,8 +16,6 @@ cd $src
 : "${MAX_THREADS:=32}"
 : "${REPEAT_BATCH:=5}"
 : "${REPEAT_METHOD:=1}"
-# SLURM specific parameters
-: "${SLURM_NTASKS:=4}"
 # Define macros (dont forget to add here)
 DEFINES=(""
 "-DTYPE=$TYPE"
@@ -39,9 +25,8 @@ DEFINES=(""
 )
 
 # Run
-MPIRUN="mpirun -np $SLURM_NTASKS --map-by node --bind-to none --report-bindings"
-mpic++ ${DEFINES[*]} -std=c++17 -O3 -fopenmp main.cxx -o "a$1.out"
-stdbuf --output=L $MPIRUN ./"a$1.out" 2>&1 | tee -a "$out"
+nvcc ${DEFINES[*]} -std=c++17 -O3 -Xcompiler -fopenmp main.cu -o "a$1.out"
+stdbuf --output=L ./"a$1.out" 2>&1 | tee -a "$out"
 
 # Signal completion
 curl -X POST "https://maker.ifttt.com/trigger/puzzlef/with/key/${IFTTT_KEY}?value1=$src$1"
